5b73ed0be6c4eee7881293bfac14610e5f7c8ae4
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-15 15:02:33 +0100

    creata e testata classe libro, no eccezioni

--- src/org/lessons/java/Libro.java
+++ src/org/lessons/java/Libro.java
@@ -1,5 +1,65 @@
 package org.lessons.java;
 
 public class Libro {
+	
+	private String titolo;
+	private String autore;
+	private String editore;
+	private int pagine;
+	
+	public Libro (String titolo, String autore, String editore, int pagine) {
+		setTitolo(titolo);
+		setAutore(autore);
+		setEditore(editore);
+		setPagine(pagine);
+		
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+
+	public String getEditore() {
+		return editore;
+	}
+
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+
+	public int getPagine() {
+		return pagine;
+	}
+
+	public void setPagine(int pagine) {
+		this.pagine = pagine;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Libro:\n"
+				+ "titolo: " + getTitolo() + "\n"
+				+ "autore: "+ getAutore() + "\n"
+				+ "editore: "+ getEditore() + "\n"
+				+ "numero pagine: "+ getPagine();
+	}
+	
+	
+	
+	
+	
 
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,15 @@
 package org.lessons.java;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		
+		Libro l1 = new Libro ("lo hobbit", "tolkien", "mondadori", 300);
+		
+		
+		
+		
+		
+		System.out.println("Libro:\n" + l1);
+	}
 }
