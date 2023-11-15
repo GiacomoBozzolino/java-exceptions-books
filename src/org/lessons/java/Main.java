a906778d0731bd057417d10b69b7be21793633ba
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-15 17:08:57 +0100

    aggiunta lettura da file txt

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,26 +1,22 @@
 package org.lessons.java;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	static final File myFile = new File("./my-file.txt");
+    static final File myFile = new File("./my-file.txt");
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
         System.out.print("Quanti libri vuoi inserire? ");
         int BOOK_COUNT = Integer.parseInt(in.nextLine());
 
         Libro[] books = new Libro[BOOK_COUNT];
-        int booksIndex = 0;
 
-        while (booksIndex < BOOK_COUNT) {
-
+        for (int booksIndex = 0; booksIndex < BOOK_COUNT; booksIndex++) {
             System.out.println("\nLibro " + (booksIndex + 1));
             System.out.print("Inserisci il titolo: ");
             String strTitolo = in.nextLine();
@@ -34,54 +30,41 @@ public class Main {
             System.out.print("Inserisci il nome dell'editore: ");
             String strEditore = in.nextLine();
 
-            Libro b= null;
-			try {
-				b = new Libro(strTitolo, numeroPagine, strAutore, strEditore);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("\nError in book: " + e.getMessage());
-			}
-            books[booksIndex++] = b;
+            Libro l = null;
+            try {
+               l = new Libro(strTitolo, numeroPagine, strAutore, strEditore);
+                books[booksIndex] = l;
+            } catch (Exception e) {
+                System.out.println("\nError in book: " + e.getMessage());
+                booksIndex--; // Se c'è un errore, decrementa l'indice per reinserire i dati corretti
+            }
         }
 
         in.close();
 
-       // scrittura su file
-        
-FileWriter myWriter = null;
-		
-		try {
-			
-			myWriter = new FileWriter(myFile);
-			
-			for (int i = 0; i < books.length; i++) {
-				
-				Libro b = books[i];
-				
-				myWriter.write("Libro " + (i+1) + "\n"
-						+ "\nTitolo: " + b.getTitolo() + "\n"
-						+ "Numero pagine: " + b.getNumeroPagine() + "\n"
-						+ "Autore: " + b.getAutore() + "\n"
-						+ "Editore: " + b.getEditore()
-						+ "\n-----------------------------\n"
-						);
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println("Error writing file: " + e.getMessage());
-			
-		} finally {
-			
-			if (myWriter != null) {
-				try {
-					myWriter.close();
-				} catch (IOException e) { }
-			}
-		}
-		
-		
-        
+        // Scrittura su file
+        try (FileWriter myWriter = new FileWriter(myFile)) {
+            for (int i = 0; i < BOOK_COUNT; i++) {
+                Libro b = books[i];
+                myWriter.write("Libro " + (i + 1) + "\n"
+                        + "\nTitolo: " + b.getTitolo() + "\n"
+                        + "Numero pagine: " + b.getNumeroPagine() + "\n"
+                        + "Autore: " + b.getAutore() + "\n"
+                        + "Editore: " + b.getEditore()
+                        + "\n\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+
+        // Lettura da file e stampa a console
+        try (Scanner reader = new Scanner(myFile)) {
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
-
